9b2eedf41c7bb28cbb99254e2f215cf303437af0
paulohenrique085 <paulohnq85@outlook.com>
2021-03-18 20:42:09 -0300

    tela ok

--- lib/Home.dart
+++ lib/Home.dart
@@ -10,17 +10,66 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: EdgeInsets.only(top: 20),
+        padding: EdgeInsets.only(top: 35),
         child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                width: 200,
-                height: 60,
-                child: Image.asset("assets/icone.png"),
-              )
-            ],
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: 200,
+                  height: 140,
+                  child: Image.asset("assets/icone.png"),
+                ),
+                Center(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 30, left: 30),
+                    child: TextField(
+                      //retirando linha textfild
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Preço Álcool  ex: 3.58",
+                      ),
+                      //estilo do texto que entra
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 30, left: 30),
+                    child: TextField(
+                      //retirando linha textfild
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Preço Gasolina  ex: 3.32",
+                      ),
+                      //estilo do texto que entra
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 32, left: 15),
+                  child: Container(
+                    width: 200,
+                    height: 52,
+                    child: RaisedButton(
+                      //arredondando bordas do botão
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12)),
+                      color: Color(0xFFd66055),
+                      onPressed: () {},
+                      child: Text(
+                        "Calcular",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
