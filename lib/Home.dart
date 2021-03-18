f2be00f5a1c8f1742e08ca4d483e494574b3bf55
paulohenrique085 <paulohnq85@outlook.com>
2021-03-18 20:18:12 -0300

    pondo image no pubyspac

--- lib/Home.dart
+++ lib/Home.dart
@@ -8,6 +8,22 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.only(top: 20),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: 200,
+                height: 60,
+                child: Image.asset("assets/icone.png"),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
